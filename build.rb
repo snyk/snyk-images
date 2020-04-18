3f2aa9fb79ce42ce28dacf5932cd136fe7c58a1a
Gareth Rushgrove <gareth@morethanseven.net>
2020-04-18 09:27:52 +0100

    Generate the linux actions

--- build.rb
+++ build.rb
@@ -3,10 +3,15 @@
 require "erb"
 require "fileutils"
 
-@images = [
-#  ["node",  "node"],
-  ["node:10", "node-10"]
-]
+
+@images = []
+File.open("linux").each do |line|
+  base,tag = line.split
+  @images.append [base, tag ? tag : base]
+end
+
+@seed = @images.shift
+
 templatename = File.join("_templates", "build.yml.erb")
 renderer = ERB.new(File.read(templatename))
 File.open(".github/workflows/build.yml", "w") { |file| file.puts renderer.result() }
