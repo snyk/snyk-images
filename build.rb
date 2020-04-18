b9902329643d063981b1b03240ac3f292682a504
Gareth Rushgrove <gareth@morethanseven.net>
2020-04-18 08:46:07 +0100

    Interim work on generating a matrix build for the Actions

---
+++ build.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require "erb"
+require 'fileutils'
+
+@images = [
+  ["node",  "node"],
+  ["node:10", "node-10"]
+]
+templatename = File.join("_templates", "build.yml.erb")
+renderer = ERB.new(File.read(templatename))
+File.open(".github/workflows/build.yml", "w") { |file| file.puts renderer.result() }
